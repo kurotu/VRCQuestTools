3846eeabe243b2199da13f6517b2c32ab7e46789
kurotu <kurotu@gmail.com>
2020-09-19 20:39:19 +0900

    Refactor tests

--- Tests/Editor/GenericMaterialTests.cs
+++ Tests/Editor/GenericMaterialTests.cs
@@ -9,24 +9,12 @@ namespace KRTQuestTools
 {
     public class GenericMaterialTests
     {
-        private const string FixturesFolder = "Assets/KRT/KRTQuestTools/Tests/Fixtures";
-        private const string MaterialsFolder = FixturesFolder + "/Materials";
-        private const string TexturesFolder = FixturesFolder + "/Textures";
-
-        private Material LoadMaterial(string file)
-        {
-            var material = AssetDatabase.LoadAssetAtPath<Material>(MaterialsFolder + "/" + file);
-            Assert.NotNull(material);
-            return material;
-        }
-
         [Test]
         public void StandardNoEmission()
         {
-            var material = LoadMaterial("Standard_NoEmission.mat");
-            var generic = MaterialUtils.CreateWrapper(material);
-            using(var image = generic.CompositeLayers())
-            using (var original = new MagickImage(TexturesFolder + "/albedo_1024px.png"))
+            var wrapper = TestUtils.LoadMaterialWrapper("Standard_NoEmission.mat");
+            using (var image = wrapper.CompositeLayers())
+            using (var original = TestUtils.LoadMagickImage("albedo_1024px.png"))
             {
                 var result = image.CompareTo(original);
                 Assert.AreEqual(result, 0);
@@ -36,11 +24,10 @@ namespace KRTQuestTools
         [Test]
         public void StandardEmission()
         {
-            var material = LoadMaterial("Standard_Emission.mat");
-            var generic = MaterialUtils.CreateWrapper(material);
-            using (var image = generic.CompositeLayers())
-            using (var main = new MagickImage(TexturesFolder + "/albedo_1024px.png"))
-            using (var emission = new MagickImage(TexturesFolder + "/emission_1024px.png"))
+            var wrapper = TestUtils.LoadMaterialWrapper("Standard_Emission.mat");
+            using (var image = wrapper.CompositeLayers())
+            using (var main = TestUtils.LoadMagickImage("albedo_1024px.png"))
+            using (var emission = TestUtils.LoadMagickImage("emission_1024px.png"))
             {
                 main.Composite(emission, CompositeOperator.Screen);
                 var result = image.CompareTo(main);
---
+++ Tests/Editor/TestUtils.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ImageMagick;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace KRTQuestTools
+{
+    static class TestUtils
+    {
+        internal const string FixturesFolder = "Assets/KRT/KRTQuestTools/Tests/Fixtures";
+        internal const string MaterialsFolder = FixturesFolder + "/Materials";
+        internal const string TexturesFolder = FixturesFolder + "/Textures";
+
+        internal static Material LoadMaterial(string file)
+        {
+            var material = AssetDatabase.LoadAssetAtPath<Material>(MaterialsFolder + "/" + file);
+            Assert.NotNull(material);
+            return material;
+        }
+
+        internal static IMaterialWrapper LoadMaterialWrapper(string file)
+        {
+            var material = LoadMaterial(file);
+            var wrapper = MaterialUtils.CreateWrapper(material);
+            Assert.NotNull(wrapper);
+            return wrapper;
+        }
+
+        internal static MagickImage LoadMagickImage(string file)
+        {
+            var image = new MagickImage(TexturesFolder + "/" + file);
+            Assert.NotNull(image);
+            return image;
+        }
+    }
+}
