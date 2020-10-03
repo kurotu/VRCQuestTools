e74ffdf8c4be2a41dbb5fd369815a7c6cc83ef2b
kurotu <kurotu@gmail.com>
2020-10-04 01:13:39 +0900

    Add VertexColorRemover

---
+++ Editor/VertexColorRemover/VertexColorRemover.cs
@@ -0,0 +1,80 @@
+// <copyright file="VertexColorRemover.cs" company="kurotu">
+// Copyright (c) kurotu. All rights reserved.
+// </copyright>
+// <author>kurotu</author>
+// <remarks>Licensed under the MIT license.</remarks>
+
+using UnityEditor;
+using UnityEngine;
+
+namespace KRTQuestTools
+{
+    /// <summary>
+    /// 頂点カラーを消去
+    /// </summary>
+    public static class VertexColorRemover
+    {
+        private const string Tag = "VertexColorRemover";
+
+        [MenuItem("GameObject/Remove All Vertex Colors", false, 200)]
+        private static void RemoveAllVertexColors()
+        {
+            var obj = Selection.activeGameObject;
+            RemoveAllVertexColors(obj);
+            Debug.LogFormat("[{0}] All vertex colors are removed from {1}", Tag, obj);
+        }
+
+        public static void RemoveAllVertexColors(GameObject gameObject)
+        {
+            var skinnedMeshRenderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+            foreach (var s in skinnedMeshRenderers)
+            {
+                var mesh = GetSharedMesh(s);
+                if (mesh == null)
+                {
+                    continue;
+                }
+                if (mesh.colors32.Length > 0)
+                {
+                    mesh.colors32 = null;
+                }
+            }
+            var meshRenderers = gameObject.GetComponentsInChildren<MeshRenderer>(true);
+            foreach (var m in meshRenderers)
+            {
+                var mesh = GetSharedMesh(m);
+                if (mesh == null)
+                {
+                    continue;
+                }
+                if (mesh.colors32.Length > 0)
+                {
+                    mesh.colors32 = null;
+                }
+            }
+        }
+
+        [MenuItem("GameObject/Remove All Vertex Colors", true)]
+        public static bool ValidateMenu()
+        {
+            return Selection.activeGameObject != null;
+        }
+
+        private static Mesh GetSharedMesh(Renderer r)
+        {
+            var type = r.GetType();
+            if (type == typeof(SkinnedMeshRenderer))
+            {
+                return ((SkinnedMeshRenderer)r).sharedMesh;
+            }
+
+            if (type == typeof(MeshRenderer))
+            {
+                return r.GetComponent<MeshFilter>().sharedMesh;
+            }
+
+            Debug.LogErrorFormat("[{0}] {1} is not either SkinnedMeshRenderer or MeshRenderer", Tag, r);
+            return null;
+        }
+    }
+}
---
+++ Editor/VertexColorRemover/VertexColorRemoverAutomator.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="VertexColorRemoverAutomator.cs" company="kurotu">
+// Copyright (c) kurotu. All rights reserved.
+// </copyright>
+// <author>kurotu</author>
+// <remarks>Licensed under the MIT license.</remarks>
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KRTQuestTools
+{
+    [InitializeOnLoad]
+    public class VertexColorRemoverAutomator
+    {
+        static readonly string Tag = typeof(VertexColorRemoverAutomator).Name;
+
+        static VertexColorRemoverAutomator()
+        {
+            EditorApplication.hierarchyChanged += HierarchyChanged;
+            RemoveAllVertexColorsFromAvatars(SceneManager.GetActiveScene());
+            Debug.Log($"[{Tag}] Loaded");
+        }
+
+        private static void HierarchyChanged()
+        {
+            Debug.Log($"[{Tag}] HierarchyChanged");
+            RemoveAllVertexColorsFromAvatars(SceneManager.GetActiveScene());
+        }
+
+        private static void RemoveAllVertexColorsFromAvatars(Scene scene)
+        {
+            var avatars = GetAvatars(scene);
+            foreach (var a in avatars)
+            {
+                VertexColorRemover.RemoveAllVertexColors(a.gameObject);
+            }
+        }
+
+        private static VRC.SDKBase.VRC_AvatarDescriptor[] GetAvatars(Scene scene)
+        {
+            var avatars = new List<VRC.SDKBase.VRC_AvatarDescriptor>();
+            var rootGameObjects = scene.GetRootGameObjects();
+            foreach (var obj in rootGameObjects)
+            {
+                avatars.AddRange(obj.GetComponentsInChildren<VRC.SDKBase.VRC_AvatarDescriptor>());
+            }
+            return avatars.ToArray();
+        }
+    }
+}
