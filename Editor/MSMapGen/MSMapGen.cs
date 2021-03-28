81bd1e654582a68c9750502e03cfc61123ff3ee8
kurotu <kurotu@gmail.com>
2021-03-28 18:13:38 +0900

    Add metallic smoothness map generator

---
+++ Editor/MSMapGen/MSMapGen.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="MSMapGen.cs" company="kurotu">
+// Copyright (c) kurotu. All rights reserved.
+// </copyright>
+// <author>kurotu</author>
+// <remarks>Licensed under the MIT license.</remarks>
+
+using ImageMagick;
+using UnityEditor;
+using UnityEngine;
+
+namespace KRT.VRCQuestTools
+{
+    public class MSMapGen : EditorWindow
+    {
+        Texture2D metallicMap;
+        Texture2D smoothnessMap;
+        bool invertSmoothness;
+        bool allowOverwriting;
+        readonly MSMapGenI18nBase i18n = MSMapGenI18n.Create();
+
+        internal static void ShowWindow()
+        {
+            GetWindow(typeof(MSMapGen));
+        }
+
+        private void OnGUI()
+        {
+            titleContent.text = "MS Map Generator";
+            EditorGUILayout.LabelField("Textures", EditorStyles.boldLabel);
+            metallicMap = (Texture2D)EditorGUILayout.ObjectField("Metallic", metallicMap, typeof(Texture2D), false);
+            EditorGUILayout.Space();
+            smoothnessMap = (Texture2D)EditorGUILayout.ObjectField("Smoothness", smoothnessMap, typeof(Texture2D), false);
+            EditorGUI.BeginDisabledGroup(smoothnessMap == null);
+            invertSmoothness = EditorGUILayout.Toggle(i18n.InvertSmoothness, invertSmoothness);
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.Space();
+
+            var disableGenerate = (smoothnessMap == null) && (metallicMap == null);
+            EditorGUI.BeginDisabledGroup(disableGenerate);
+            if (GUILayout.Button(i18n.Generate))
+            {
+                var dest = EditorUtility.SaveFilePanelInProject(i18n.SaveFileDialogTitle("Metallic Smoothness"), "MetallicSmoothness", "png", "Please enter a file name to save the texture to");
+                if (dest != "") // Cancel
+                {
+                    var metallicPath = metallicMap ? AssetDatabase.GetAssetPath(metallicMap) : "";
+                    var smoothPath = smoothnessMap ? AssetDatabase.GetAssetPath(smoothnessMap) : "";
+                    using (var metallic = metallicMap ?
+                        new MagickImage(AssetDatabase.GetAssetPath(metallicMap)) :
+                        new MagickImage(MagickColors.White, 2, 2))
+                    using (var smoothness = smoothnessMap ?
+                        new MagickImage(AssetDatabase.GetAssetPath(smoothnessMap)) :
+                        new MagickImage(MagickColors.White, 2, 2))
+                    using (var msMap = GenerateMetallicSmoothness(metallic, false, smoothness, invertSmoothness && smoothnessMap))
+                    {
+                        msMap.Write(dest, MagickFormat.Png32);
+                    }
+                    AssetDatabase.Refresh();
+                    var importer = AssetImporter.GetAtPath(dest) as TextureImporter;
+                    importer.sRGBTexture = false;
+                    importer.alphaIsTransparency = false;
+                    importer.alphaSource = TextureImporterAlphaSource.FromInput;
+                    importer.SaveAndReimport();
+                }
+            }
+        }
+
+        public static MagickImage GenerateMetallicSmoothness(MagickImage metallic, bool invertMetallic, MagickImage smoothness, bool invertSmoothness)
+        {
+            var width = System.Math.Max(metallic.Width, smoothness.Width);
+            var height = System.Math.Max(metallic.Height, smoothness.Height);
+            using (var m = new MagickImage(metallic))
+            using (var s = new MagickImage(smoothness))
+            using (var green = new MagickImage(MagickColors.Black, metallic.Width, metallic.Height))
+            using (var blue = new MagickImage(MagickColors.Black, metallic.Width, metallic.Height))
+            using (var rgb = new MagickImageCollection())
+            {
+                m.ColorSpace = ImageMagick.ColorSpace.Gray;
+                m.Resize(width, height);
+                if (invertMetallic) { m.Negate(); }
+                s.ColorSpace = ImageMagick.ColorSpace.Gray;
+                s.Resize(width, height);
+                if (invertSmoothness) { s.Negate(); }
+
+                rgb.Add(m);
+                rgb.Add(green);
+                rgb.Add(blue);
+                var rgba = (MagickImage)rgb.Combine(ImageMagick.ColorSpace.sRGB);
+                rgba.Alpha(AlphaOption.On);
+                rgba.Composite(s, CompositeOperator.CopyAlpha);
+                return rgba;
+            }
+        }
+    }
+}
---
+++ Editor/MSMapGen/MSMapGenI18n.cs
@@ -0,0 +1,100 @@
+﻿// <copyright file="MSMapGenI18n.cs" company="kurotu">
+// Copyright (c) kurotu. All rights reserved.
+// </copyright>
+// <author>kurotu</author>
+// <remarks>Licensed under the MIT license.</remarks>
+
+namespace KRT.VRCQuestTools
+{
+    static class MSMapGenI18n
+    {
+        public static MSMapGenI18nBase Create()
+        {
+            if (System.Globalization.CultureInfo.CurrentCulture.Name == "ja-JP")
+            {
+                return new MSMapGenI18nJapanese();
+            }
+            else
+            {
+                return new MSMapGenI18nEnglish();
+            }
+        }
+    }
+
+    abstract class MSMapGenI18nBase
+    {
+        public abstract string OverwriteWarningDialogTitle { get; }
+        public abstract string OverwriteWarningDialogMessage(string artifactPath);
+        public abstract string ButtonOK { get; }
+        public abstract string ButtonCancel { get; }
+        public abstract string InvertSmoothness { get; }
+        public abstract string AllowOverwriting { get; }
+        public abstract string Select { get; }
+        public abstract string SaveFileDialogTitle(string thing);
+        public abstract string SaveFileDialogMessage { get; }
+        public abstract string SaveAs { get; }
+        public abstract string Generate { get; }
+    }
+
+    class MSMapGenI18nEnglish : MSMapGenI18nBase
+    {
+        public override string ButtonCancel => "Cancel";
+
+        public override string ButtonOK => "OK";
+
+        public override string OverwriteWarningDialogMessage(string artifactPath)
+        {
+            return $"\"{artifactPath}\" already exists. Do you want to overwrite?";
+        }
+
+        public override string OverwriteWarningDialogTitle => "Warning - VRCStandardLiteUtils";
+
+        public override string InvertSmoothness => "Invert Smoothness";
+
+        public override string AllowOverwriting => "Allow Overwriting";
+
+        public override string Select => "Select";
+
+        public override string SaveFileDialogTitle(string thing)
+        {
+            return $"Save {thing}";
+        }
+
+        public override string SaveFileDialogMessage => "Please enter a file name to save the texture to";
+
+        public override string SaveAs => "Save as";
+
+        public override string Generate => "Generate";
+    }
+
+    class MSMapGenI18nJapanese : MSMapGenI18nBase
+    {
+        public override string ButtonCancel => "キャンセル";
+
+        public override string ButtonOK => "OK";
+
+        public override string OverwriteWarningDialogMessage(string artifactPath)
+        {
+            return $"\"{artifactPath}\" が既に存在します。上書きしますか？";
+        }
+
+        public override string OverwriteWarningDialogTitle => "警告 - VRCStandardLiteUtils";
+
+        public override string InvertSmoothness => "Smoothness を反転";
+
+        public override string AllowOverwriting => "上書きを許可";
+
+        public override string Select => "選択";
+
+        public override string SaveFileDialogTitle(string thing)
+        {
+            return $"{thing} を保存";
+        }
+
+        public override string SaveFileDialogMessage => "テクスチャの保存先を選択してください";
+
+        public override string SaveAs => "保存先";
+
+        public override string Generate => "生成";
+    }
+}
