0fbe64ca0cffe444f62d365cba77332010c4cece
kurotu <kurotu@gmail.com>
2021-03-28 15:41:39 +0900

    Refactor VRCSDKUtils.RemoveUnsupportedComponentsInChildren

--- Editor/VRCSDKUtils.cs
+++ Editor/VRCSDKUtils.cs
@@ -12,6 +12,16 @@ namespace KRT.VRCQuestTools
 {
     static class VRCSDKUtils
     {
+        static readonly System.Type[] UnsupportedComponentTypes = new System.Type[] {
+            GetType("DynamicBoneColliderBase"), GetType("DynamicBone"), // DynamicBone may be missing
+            typeof(Cloth),
+            typeof(Camera),
+            typeof(Light),
+            typeof(AudioSource),
+            typeof(Joint), typeof(Rigidbody),typeof(Collider),
+            typeof(UnityEngine.Animations.IConstraint)
+        }.Where(e => e != null).ToArray();
+
         internal static bool IsAvatar(GameObject obj)
         {
             if (obj == null) { return false; }
@@ -19,33 +29,25 @@ namespace KRT.VRCQuestTools
             return true;
         }
 
+        internal static Component[] GetUnsupportedComponentsInChildren(GameObject gameObject, bool includeInactive)
+        {
+            return UnsupportedComponentTypes.SelectMany(type => gameObject.GetComponentsInChildren(type, includeInactive)).ToArray();
+        }
+
         internal static void RemoveUnsupportedComponentsInChildren(GameObject gameObject, bool includeInactive, bool canUndo = false)
         {
-            var types = new System.Type[] {
-                GetType("DynamicBoneColliderBase"), GetType("DynamicBone"), // DynamicBone may be missing
-                typeof(Cloth),
-                typeof(Camera),
-                typeof(Light),
-                typeof(AudioSource),
-                typeof(Joint), typeof(Rigidbody),typeof(Collider),
-                typeof(UnityEngine.Animations.IConstraint)
-            }.Where(e => e != null);
-            foreach (var type in types)
+            foreach (var c in GetUnsupportedComponentsInChildren(gameObject, includeInactive))
             {
-                var components = gameObject.GetComponentsInChildren(type, includeInactive);
-                foreach (var c in components)
+                var message = $"[VRCQuestTools] Removed {c.GetType().Name} from {c.gameObject.name}";
+                if (canUndo)
                 {
-                    var message = $"[VRCQuestTools] Removed {c.GetType().Name} from {c.gameObject.name}";
-                    if (canUndo)
-                    {
-                        Undo.DestroyObjectImmediate(c);
-                    }
-                    else
-                    {
-                        Object.DestroyImmediate(c);
-                    }
-                    Debug.Log(message);
+                    Undo.DestroyObjectImmediate(c);
                 }
+                else
+                {
+                    Object.DestroyImmediate(c);
+                }
+                Debug.Log(message);
             }
         }
 
