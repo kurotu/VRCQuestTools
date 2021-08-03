322716a2bef6d28d71af91f989ffe77a82bd1c3a
kurotu <kurotu@gmail.com>
2021-08-04 00:18:16 +0900

    Version 0.5.2

--- Editor/VRCQuestTools.cs
+++ Editor/VRCQuestTools.cs
@@ -15,7 +15,7 @@ namespace KRT.VRCQuestTools
     [InitializeOnLoad]
     public static class VRCQuestTools
     {
-        public const string Version = "0.5.1";
+        public const string Version = "0.5.2";
 
         static class MenuPaths
         {
