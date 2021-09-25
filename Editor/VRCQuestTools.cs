bac9157ab963115eecde0fc6ae7e781705a38c07
kurotu <kurotu@gmail.com>
2021-09-25 22:24:12 +0900

    Version 1.0.1

--- Editor/VRCQuestTools.cs
+++ Editor/VRCQuestTools.cs
@@ -22,7 +22,7 @@ namespace KRT.VRCQuestTools
         /// <summary>
         /// VRCQuestTools version (semver).
         /// </summary>
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         /// <summary>
         /// Days to delay update notification.
