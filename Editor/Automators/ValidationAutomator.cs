3794dcdeda0b92b04a769e71e7270214b31b4fa2
kurotu <kurotu@gmail.com>
2022-03-01 02:18:03 +0900

    Remove test codes

--- Editor/Automators/ValidationAutomator.cs
+++ Editor/Automators/ValidationAutomator.cs
@@ -3,13 +3,11 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 // </copyright>
 
-using KRT.VRCQuestTools.Models;
 using KRT.VRCQuestTools.Models.Validators;
 using KRT.VRCQuestTools.Models.VRChat;
 using KRT.VRCQuestTools.Utils;
 using KRT.VRCQuestTools.Views;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace KRT.VRCQuestTools.Automators
@@ -46,44 +44,6 @@ namespace KRT.VRCQuestTools.Automators
                     }
                 }
             }
-
-            if (!Validate(scene))
-            {
-                NotificationWindow.instance.RegisterNotification("active", new NotificationItem(() =>
-                {
-                    GUILayout.Label("No active avatars in scene!");
-                    return GUILayout.Button("Dismiss");
-                }));
-                NotificationWindow.instance.RegisterNotification("active1", new NotificationItem(() =>
-                {
-                    GUILayout.Label("No active avatars in scene!");
-                    return GUILayout.Button("Dismiss");
-                }));
-                NotificationWindow.instance.RegisterNotification("active2", new NotificationItem(() =>
-                {
-                    GUILayout.Label("No active avatars in scene!");
-                    return GUILayout.Button("Dismiss");
-                }));
-            }
-            else
-            {
-                NotificationWindow.instance.RemoveNotification("active");
-                NotificationWindow.instance.RemoveNotification("active1");
-                NotificationWindow.instance.RemoveNotification("active2");
-            }
-        }
-
-        private static bool Validate(Scene scene)
-        {
-            var avatars = VRCSDKUtility.GetAvatarsFromScene(scene);
-            foreach (var a in avatars)
-            {
-                if (a.gameObject.activeInHierarchy)
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
