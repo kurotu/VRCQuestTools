c6db38cefdaaf12625305541684ba97d06ee4663
kurotu <kurotu@gmail.com>
2022-09-24 14:49:47 +0900

    Add date in bump.sh

--- .scripts/bump.sh
+++ .scripts/bump.sh
@@ -2,7 +2,7 @@
 set -eu
 
 VERSION="${1}"
-sed -i -b -e "s/\[Unreleased\]/\[${VERSION}\]/g" CHANGELOG*.md
+sed -i -b -e "s/\[Unreleased\]/\[${VERSION}\] - $(date -I)/g" CHANGELOG*.md
 sed -i -b -e "s/\"version\": \".*\"/\"version\": \"${VERSION}\"/g" package.json
 sed -i -b -e "s/string Version = \".*\"/string Version = \"${VERSION}\"/g" Editor/VRCQuestTools.cs
 git commit -am "Version ${VERSION}"
