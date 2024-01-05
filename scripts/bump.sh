fcf13e5276586d132043fb1c8f04aedd68d83f26
kurotu <kurotu@gmail.com>
2024-01-05 18:30:05 +0900

    Fix bump.sh for v2 layout

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -2,9 +2,10 @@
 set -eu
 
 VERSION="${1}"
+PACKAGE_DIR="Packages/com.github.kurotu.vrc-quest-tools"
 sed -i -b -e "s/\[Unreleased\]/\[${VERSION}\] - $(date -I)/g" CHANGELOG*.md
-sed -i -b -e "s/\"version\": \".*\"/\"version\": \"${VERSION}\"/g" package.json
-sed -i -b -e "s|download/.*.zip|download/v${VERSION}/com.github.kurotu.vrc-quest-tools-${VERSION}.zip|g" package.json
-sed -i -b -e "s/string Version = \".*\"/string Version = \"${VERSION}\"/g" Editor/VRCQuestTools.cs
+sed -i -b -e "s/\"version\": \".*\"/\"version\": \"${VERSION}\"/g" "${PACKAGE_DIR}/package.json"
+sed -i -b -e "s|download/.*.zip|download/v${VERSION}/com.github.kurotu.vrc-quest-tools-${VERSION}.zip|g" "${PACKAGE_DIR}/package.json"
+sed -i -b -e "s/string Version = \".*\"/string Version = \"${VERSION}\"/g" "${PACKAGE_DIR}/Editor/VRCQuestTools.cs"
 git commit -am "Version ${VERSION}"
 git tag "v${VERSION}"
