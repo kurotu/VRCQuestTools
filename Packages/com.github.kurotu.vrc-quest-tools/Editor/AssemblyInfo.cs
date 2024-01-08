a7c0b33373af8219f0c267099739417a6aa5f309
kurotu <kurotu@gmail.com>
2024-01-08 16:36:05 +0900

    Add NDMF asmdef

--- Packages/com.github.kurotu.vrc-quest-tools/Editor/AssemblyInfo.cs
+++ Packages/com.github.kurotu.vrc-quest-tools/Editor/AssemblyInfo.cs
@@ -6,6 +6,7 @@
 #if UNITY_EDITOR
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("VRCQuestTools-Editor-Ndmf")]
 [assembly: InternalsVisibleTo("VRCQuestTools-DebugUtil-Editor")]
 [assembly: InternalsVisibleTo("VRCQuestTools-EditorTests")]
 [assembly: InternalsVisibleTo("VRCQuestToolsExtra-Editor")]
