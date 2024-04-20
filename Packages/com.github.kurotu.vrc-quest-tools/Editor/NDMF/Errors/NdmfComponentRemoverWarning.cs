4a9f75150e2ac5b6f116a812fb5968985fb39617
kurotu <kurotu@gmail.com>
2024-04-20 15:28:25 +0900

    Move NdmfComponentRemoverWarning to Errors folder

---
+++ Packages/com.github.kurotu.vrc-quest-tools/Editor/NDMF/Errors/NdmfComponentRemoverWarning.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using nadena.dev.ndmf;
+#if VQT_HAS_NDMF_ERROR_REPORT
+using nadena.dev.ndmf.localization;
+#else
+using KRT.VRCQuestTools.Ndmf.Dummy;
+#endif
+
+namespace KRT.VRCQuestTools.Ndmf
+{
+    /// <summary>
+    /// Warning for removing unsupported component.
+    /// </summary>
+    internal class NdmfComponentRemoverWarning : SimpleError
+    {
+        private string componentTypeName;
+        private string objectName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NdmfComponentRemoverWarning"/> class.
+        /// </summary>
+        /// <param name="componentType">Remove component type.</param>
+        /// <param name="objectReference">ObjectReference for removed component.</param>
+        public NdmfComponentRemoverWarning(System.Type componentType, ObjectReference objectReference)
+        {
+            this.componentTypeName = componentType.Name;
+            this.objectName = objectReference.Object.name;
+            _references = new List<ObjectReference> { objectReference };
+        }
+
+        /// <inheritdoc/>
+        public override ErrorSeverity Severity => ErrorSeverity.Information;
+
+        /// <inheritdoc/>
+        public override Localizer Localizer => NdmfLocalizer.Instance;
+
+        /// <inheritdoc/>
+        public override string TitleKey => NdmfLocalizer.RemovedUnsupportedComponentTitle;
+
+        /// <inheritdoc/>
+        public override string DetailsKey => NdmfLocalizer.RemovedUnsupportedComponentDescription;
+
+        /// <inheritdoc/>
+        public override string[] DetailsSubst => new[] { componentTypeName, objectName };
+    }
+}
