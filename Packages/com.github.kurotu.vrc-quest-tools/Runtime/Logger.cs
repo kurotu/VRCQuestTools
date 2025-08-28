3e5363b5e0b82308d87691a223e82aa47363749a
kurotu <kurotu@gmail.com>
2025-08-28 13:37:22 +0900

    Add LogDebug

--- Packages/com.github.kurotu.vrc-quest-tools/Runtime/Logger.cs
+++ Packages/com.github.kurotu.vrc-quest-tools/Runtime/Logger.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace KRT.VRCQuestTools
@@ -9,6 +11,11 @@ namespace KRT.VRCQuestTools
     public static class Logger
     {
         private const string Prefix = "[VRCQuestTools] ";
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use debug logging.
+        /// </summary>
+        public static bool UseDebug { get; set; }
 
         /// <summary>
         /// Logs a message.
@@ -57,6 +64,29 @@ namespace KRT.VRCQuestTools
         public static void LogException(Exception exception, UnityEngine.Object context = null)
         {
             Debug.LogException(exception, context);
+        }
+
+        /// <summary>
+        /// Logs a debug message.
+        /// </summary>
+        /// <param name="message">The debug message to log.</param>
+        /// <param name="context">The context object for the log entry.</param>
+        /// <param name="filePath">The source file path of the caller.</param>
+        /// <param name="lineNumber">The line number in the source file at which the method is called.</param>
+        /// <param name="memberName">The name of the calling member.</param>
+        [HideInCallstack]
+        public static void LogDebug(
+            object message,
+            UnityEngine.Object context = null,
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string memberName = "")
+        {
+            if (UseDebug)
+            {
+                var fileName = Path.GetFileName(filePath);
+                Log($"(debug) [{fileName}:{lineNumber}] {memberName}: {message}", context);
+            }
         }
     }
 }
